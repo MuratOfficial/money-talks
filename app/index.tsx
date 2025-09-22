0787b5d8760d7a92a8f1ea8756270a3fd85c665f
MuratOfficial <toimetm@gmail.com>
2025-09-22 17:16:09 +0500

    added Supabase

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,29 @@
 
+import { supabase } from '@/lib/supabase';
+import { Session } from '@supabase/supabase-js';
 import { Redirect } from 'expo-router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+
 
 export default function Index() {
-  return <Redirect href="/main" />;
+
+    const [session, setSession] = useState<Session | null>(null);
+
+    useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  if(session && session.user){
+    return <Redirect href="/main" />;
+  }else{
+    return <Redirect href="/(auth)/login" />;
+  }
+
+  
 }
